3eda84e2888e6253e2d6a852fc59e4cd037b0535
Saikrithiha <saikrithiha6@gmail.com>
2025-10-28 23:45:48 +0530

    commit

--- src/test/java/com/app/base/BaseClass.java
+++ src/test/java/com/app/base/BaseClass.java
@@ -1,6 +1,8 @@
 package com.app.base;
 
 import org.openqa.selenium.WebDriver;
+
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
